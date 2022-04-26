37ace2964b404bad1d78aab7446ee8102fb55323
ngoctuct-cq <ngoctuct@gmail.com>
2022-04-26 14:34:41 +0700

    cap nhat thuvien

--- bidvrunlib.py
+++ bidvrunlib.py
@@ -13,8 +13,6 @@ import urllib3
 import uuid
 import json 
 import pandas as pd 
-from pandas.io.json import json_normalize 
-  
 
 
 def sign_string( to_sign):
@@ -86,19 +84,3 @@ def DownloadRunnerData(teamID ):
          
         db.append(pd.json_normalize(data))
     return pd.concat(db)
-
-dp = DownloadRunnerData(175)
-dp.reset_index()
-cbphong = pd.read_excel('cb_phongban.xlsx')
-dp ['phong'] = dp ['runVandongvienId'].map(cbphong.set_index('runVandongvienId')['phong'])
-dp ['hoten'] = dp ['runVandongvienId'].map(cbphong.set_index('runVandongvienId')['runVdvHoten'])
-dp ['ngaydl'] = date.today()
-#output[['soHoatDong','thoiGian','quangDuong','mucDongGop','dongGop','dongGopQuangDuong','dongGopSuKien','dongGopKhuyenMai']] = output[['soHoatDong','thoiGian','quangDuong','mucDongGop','dongGop','dongGopQuangDuong','dongGopSuKien','dongGopKhuyenMai']].astype(int)
-
-
-headers=['ngaydl','hoten','phong','gioiTinh','ebib','soHoatDong','thoiGian','quangDuong','mucDongGop','dongGop','dongGopQuangDuong','dongGopSuKien','xepHangDoi','xepHangDoiGioiTinh','dongGopKhuyenMai','runVandongvienId','runVdvHoten','runVdvNickname','nickname','runVdvGioitinh','runVdvAnhTen','runDoiTen']
-
-checl = dp[headers] 
-ngayhientai = date.today().strftime("%d-%m-%Y")
-
-checl.to_excel(f"data\cb_{ngayhientai}.xlsx",index=False)
